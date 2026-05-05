41a512190e43e71648b719f8d2e14939176f390b
Tushar911000ei48e8e8e <tusharchauhan908110@gmail.com>
2026-05-05 16:35:05 +0000

    dataType

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,28 @@
+"use strict"; // treat all js as newr version
+
+// alert (3 + 3) // we are using nodejs , not browser
+
+console.log(3 
+    + 3) // code readability should be high 
+
+console.log("Tushar")
+
+
+let  name ="Tushar"
+let age = 18
+let isLoggedIn = false
+
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+// object 
+
+
+console.log (typeof undefined); // undefined
+console.log(typeof null); // object
